e725a127a921010886fcfb8220a4dee4c3dc2bae
Kelompok2 <nareswari219@gmail.com>
2021-05-21 08:16:04 +0700

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,33 @@
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php echo base_url() ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php echo base_url() ?>assets/js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="<?php echo base_url() ?>assets/vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="<?php echo base_url() ?>assets/js/demo/chart-area-demo.js"></script>
+  <script src="<?php echo base_url() ?>assets/js/demo/chart-pie-demo.js"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <script>
+  $(document).ready(function(){
+    $('.dropdown-submenu a.test').on("click", function(e){
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
+  </script>
+
+</body>
+
+</html>
